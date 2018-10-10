811ad7432456eec97ffd308a96915e05dffbb820
Joseph Anthony Scott <scottjoe1996@josephs-mbp.eduroam.hull.ac.uk>
2018-10-10 14:38:37 +0100

    :x

---
+++ README.md
@@ -0,0 +1,1 @@
+This program allows the user to convert currency between US dollars, Pound sterling or Euro
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,5 +1,29 @@
+import com.company.CurrencyConversionMenu;
+import com.company.InvalidChoice;
 import org.testng.annotations.Test;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-public class CurrencyCalculationTest {
+public class MainTest {
 
+    @Test
+    public void tryCatchCurrencyFrom() {
+        int currencyFrom = 0;
+
+        CurrencyConversionMenu currencyConversionMenu = new CurrencyConversionMenu();
+
+        try {
+            currencyFrom = new Scanner(System.in).nextInt();
+        } catch (InputMismatchException exc) {
+            System.out.println(exc);
+        }
+        try {
+          currencyConversionMenu.displaySelectCurrencyMenu(currencyFrom);
+        if (currencyConversionMenu.isValid(currencyFrom)) {
+          throw new InvalidChoice();
+        }
+        } catch (InvalidChoice exc) {
+            System.out.print(exc);
+        }
+    }
 }
